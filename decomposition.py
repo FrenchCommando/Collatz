97d6d272ed69f26a43a865dd585ed73d509872a9
FrenchCommando <martialrengmail.com>
2020-12-17 15:55:32 -0500

    cleaner display with intermediate print argument

--- decomposition.py
+++ decomposition.py
@@ -14,13 +14,22 @@ def magenta(text, **kwargs):
     print(colored(text=text, color="magenta"), **kwargs)
 
 
+def cyan(text, **kwargs):
+    print(colored(text=text, color="cyan"), **kwargs)
+
+
 def hcount(a):
     count = a.count("1")
-    # green(text=count, end="\t")
     return count
 
 
-def process(a):
+def lcount(a):
+    first_one = next((i for i, c in enumerate(a) if a[i] == "1"))
+    last_one = next((i for i, c in enumerate(a[::-1]) if a[-i] == "1"))
+    return len(a) - first_one - last_one + 1
+
+
+def process(a, print_intermediate):
     while a[-2] != "0" or a[-1] != "0":
         a += "0"
     s = list(a)
@@ -36,34 +45,46 @@ def process(a):
     for i in range(len(s) - 1, -1, -1):
         if s[i] == "1":
             add(index=i+1)
-    green(text="".join(s).replace("0", " "))
+    if print_intermediate:
+        green(text="".join(s).replace("0", " "))
 
     # that's the +1
     first_one = next((i for i in range(len(s)) if s[i] == "1"))
     add(index=first_one)
-    # magenta(text="".join(s).replace("0", " "))
 
     return "".join(s)
 
 
-def decompose(n):
+def decompose(n, print_intermediate):
     b = f"{n:b}"
     red(f"Input number {n}\tBinary representation {b}")
     a = b[::-1]
     red(f"Regular binary {b}\tInverse binary {a}")
     count = 0
-    red(text=a.replace("0", " "))
-    while hcount(a) != 1:
-        a = process(a)
-        red(text=a.replace("0", " "))
-        count += 1
+    red(text=a.replace("0", " "), end="\t")
+    hc = hcount(a=a)
+    magenta(text=f"\t\t\t{hc}", end="\t")
+    cyan(text=f"\t\t\t{lcount(a=a)}", end="\t")
     print()
+    while hc != 1:
+        a = process(a, print_intermediate)
+        hc = hcount(a)
+        red(text=a.replace("0", " "), end="\t")
+        magenta(text=f"\t\t\t{hc}", end="\t")
+        cyan(text=f"\t\t\t{lcount(a=a)}", end="\t")
+        print()
+        count += 1
     print(f"Count:\t{count} for\t{n}")
 
 
 if __name__ == '__main__':
-    if len(sys.argv) == 1:
-        n_value = 1000000
-    else:
-        n_value = int(sys.argv[1])
-    decompose(n=n_value)
+    n_value = 1000000
+    print_intermediate_value = False
+    if len(sys.argv) == 2:
+        n_value = int(sys.argv[-1])
+    elif len(sys.argv) == 3:
+        n_value = int(sys.argv[-1])
+        if sys.argv[-2] == "1":
+            print_intermediate_value = True
+
+    decompose(n=n_value, print_intermediate=print_intermediate_value)
--- readme.md
+++ readme.md
@@ -7,6 +7,11 @@ What happens to iterative applications of f on natural integers.
 
 # Dynamic view using dyadic decomposition
 
+args:
+- integer string in decimal representation
+- precede with 1 argument if display intermediate value 
+ - otherwise, no intermediate value
+
 Let's see how it goes
 
 - red is the actual values
