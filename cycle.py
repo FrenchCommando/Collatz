a29533ff8b23153324ff125b828bfc2d648d792c
FrenchCommando <martialrengmail.com>
2021-01-07 11:55:44 -0500

    cycle with more functions

--- cycle.py
+++ cycle.py
@@ -2,7 +2,7 @@ import argparse
 from src.cycle_check import check
 
 
-n_value = ['2', '1', '2', '1', '2', '1', '1', '1', '4']
+n_value = ['2', '1', '2', '2', '1', '1', '1', '4', '1', '2', '1', '2', '2', '1', '1', '5', '4', '1', '2', '1']
 
 parser = argparse.ArgumentParser()
 parser.add_argument('number', help='starting number', default=n_value, nargs='*')
--- src/cycle_check.py
+++ src/cycle_check.py
@@ -14,22 +14,46 @@ from sympy.core import Rational
 
 def build_numerator(s, denominator=1):
     k = len(s)
-    numerator = 0
-    f = 0
-    for i, si in enumerate(s):
-        numerator += 3 ** (k - 1 - i) * 2 ** f
-        f += si
-    # red(text=f"{numerator}")
 
-    numerator_factors = factorint(n=numerator)
-    numerator_factors_string = " ".join([f"{k}   " * kk for k, kk in reversed(numerator_factors.items())])
-    if 2 not in numerator_factors:
-        magenta(text=s, end="     ")
+    def build_internal(s_int):
+        numerator = 0
+        f = 0
+        for ii, si in enumerate(s_int):
+            numerator += 3 ** (k - 1 - ii) * 2 ** f
+            f += si
+        numerator_factors = factorint(n=numerator)
+        numerator_factors_string = " ".join([f"{kkk}   " * kk
+                                             for kkk, kk in reversed(numerator_factors.items())])
+        magenta(text=s_int, end="     ")
         yellow(text=f"Numerator\t{numerator}", end="     ")
         cyan(text=f"{numerator / denominator:.5f}", end="     ")
         yellow(text=f"{numerator_factors_string}", end="     ")
         print()
-    return numerator
+        return numerator
+
+    for i in range(1, k):
+        build_internal(s_int=s[i:]+s[:i])
+
+    return build_internal(s_int=s)
+
+
+def check_bounds(n, k):
+    red(f"n\t{n}")
+    red(f"k\t{k}")
+    if n > 2 * k:
+        cyan(text=f"Bound error:\tn exceeds 2k by {n - 2 * k}")
+        return False
+    if 2 ** n <= 3 ** k:
+        cyan(text=f"Bound error:\tn too small - negative denominator {2 ** n - 3 ** k}")
+        return False
+    return True
+
+
+def build_denominator(n, k):
+    denominator = 2 ** n - 3 ** k
+    denominator_factors = factorint(n=denominator)
+    red(f"Denominator\t{denominator}\t{denominator_factors}")
+    return denominator
 
 
 def check(s):
@@ -37,16 +61,9 @@ def check(s):
     red(s)
     n = sum(s)
     k = len(s)
-    red(f"n\t{n}")
-    red(f"k\t{k}")
-
-    denominator = 2 ** n - 3 ** k
-    denominator_factors = factorint(n=denominator)
-    red(f"Denominator\t{denominator}\t{denominator_factors}")
-
-    for i in range(1, k):
-        numerator = build_numerator(s=s[i:]+s[:i], denominator=denominator)
+    if not check_bounds(n=n, k=k):
+        return
+    denominator = build_denominator(n=n, k=k)
     numerator = build_numerator(s=s, denominator=denominator)
-
     green(f"Ratio\t{numerator / denominator}")
     green(f"Ratio\t{(Rational(numerator, denominator))}")
